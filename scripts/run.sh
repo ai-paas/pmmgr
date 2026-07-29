0d9b8628671f0950b08b4f1b4b5ca987f0f0bf00
hym <hans@kisti.re.kr>
2026-07-29 16:43:43 +0900

    Add scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,4 @@ PORT=8001
 export PATH=$PATH:$ROOT_DIR:$ROOT_DIR/pmmgr
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR:$ROOT_DIR/pmmgr
 
-# conda activate PMMgr
-
 python $ROOT_DIR/pmmgr/main.py run_server --port $PORT
